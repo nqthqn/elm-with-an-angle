4c574c56b3406d43f4566f5456633984fe18af8d
Nathan Nichols <nnichols@concentricsky.com>
2018-12-12 12:23:45 -0800

    attempt to import elm app

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,14 +1,17 @@
+console.log(__dirname + '/src/app/elm');
+
 module.exports = {
-    module: {
-      rules: [{
-        test: /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        use: {
-          loader: 'elm-webpack-loader',
-          options: {
-              cwd: __dirname + '/src/elm'
-          }
-        }
-      }]
-    }
-  };
+  module: {
+    rules: [{
+      test: /\.elm$/,
+      exclude: [/elm-stuff/, /node_modules/],
+      loader: 'elm-webpack-loader',
+      options: {
+        cwd: __dirname + '/src/app/elm'
+      },
+    }]
+  },
+  resolve: {
+    extensions: ['.elm']
+  },
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Foo } from './elm/foo';
+import { Main } from './elm/Main';
+
+const f = new Foo();
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>angular wrapper</h1><div id="main">{{f.d}}</div>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
